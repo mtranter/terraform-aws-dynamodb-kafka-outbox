6ddc3b24cb92b9fab42d75616445de3ea2649689
Mark Tranter <mark.a.tranter@gmail.com>
2023-06-24 07:41:56 +1000

    Tweak deployments

--- layer/main.tf
+++ layer/main.tf
@@ -2,7 +2,12 @@ terraform {
   backend "s3" {
     key    = "layer/terraform.tfstate"
     bucket = "terraform-aws-dynamodb-kafka-outbox"
+    region = "ap-southeast-2"
   }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
 }
 
 variable "is_beta" {
@@ -16,10 +21,20 @@ data "archive_file" "lambda_layer_payload" {
 }
 
 resource "aws_lambda_layer_version" "lambda_layer" {
-  filename     = data.archive_file.lambda_layer_payload.output_path
-  layer_name   = "dynamodb-kakfa-outbox${var.is_beta ? "-beta" : ""}}"
-  skip_destroy = true
-
+  filename            = data.archive_file.lambda_layer_payload.output_path
+  layer_name          = "dynamodb-kakfa-outbox${var.is_beta ? "-beta" : ""}}"
+  skip_destroy        = true
+  source_code_hash    = filebase64sha256(data.archive_file.lambda_layer_payload.output_path)
   compatible_runtimes = ["nodejs18.x"]
 }
 
+resource "aws_lambda_layer_version_permission" "lambda_layer_permission" {
+  count          = var.is_beta ? 0 : 1
+  layer_name     = aws_lambda_layer_version.lambda_layer.arn
+  version_number = aws_lambda_layer_version.lambda_layer.version
+  principal      = "*"
+  action         = "lambda:GetLayerVersion"
+  statement_id   = "dev-account"
+
+  skip_destroy = true
+}
