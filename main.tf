2d5821ee569c80745cb2653287ebf5613558f13a
Mark Tranter <mark.a.tranter@gmail.com>
2023-06-25 19:45:56 +1000

    Update root tf module

--- main.tf
+++ main.tf
@@ -1,29 +1,35 @@
-
 data "archive_file" "tx_outbox" {
-  output_path = "${path.module}/tx-outbox-handler.zip"
-  source_file = "${path.module}/../dist/tx-outbox-handler.js"
-  type        = "zip"
+  type                    = "zip"
+  output_path             = "${path.module}/tx-outbox-handler.zip"
+  source_content_filename = "tx-outbox-handler"
+  source_content          = <<EOF
+const { handler } = require('dynamodb-kafka-outbox')
+exports.handler = handler;
+EOF
 }
 
-module "streams_handler" {
-  source       = "github.com/mtranter/platform-in-a-box-aws//modules/terraform-aws-piab-lambda"
-  name         = "${var.project_name}-${var.environment}-${var.service_name}-tx-outbox"
-  service_name = var.project_name
-  runtime      = "nodejs18.x"
-  handler      = "tx-outbox-handler.handler"
-  filename     = data.archive_file.tx_outbox.output_path
 
+module "streams_handler" {
+  source   = "github.com/mtranter/platform-in-a-box-aws//modules/terraform-aws-piab-lambda"
+  name     = var.instance_name
+  runtime  = "nodejs18.x"
+  handler  = "${data.archive_file.tx_outbox.source_content_filename}.handler"
+  filename = data.archive_file.tx_outbox.output_path
+
+  layers     = [var.handler_layer]
   create_dlq = true
   environment_vars = {
     // kafka broker host. Split by :// to remove the protocol
-    KAFKA_BROKERS  = split("://", data.aws_ssm_parameter.kafka_cluster_endpoint.value)[1]
-    KAFKA_USERNAME = var.kafka_user_id
-    KAFKA_PASSWORD = var.kafka_user_secret
+    KAFKA_BROKERS        = join(",", var.kafka_brokers)
+    KAFKA_USERNAME       = var.kafka_user_id
+    KAFKA_PASSWORD       = var.kafka_user_secret
+    KAFKA_AUTH_MECHANISM = var.kafka_auth_mechanism
+    KAFKA_USE_SSL        = var.kafka_use_ssl
   }
 }
 
 resource "aws_iam_role_policy" "events_handler_can_dynamo" {
-  name   = "${var.project_name}-${var.environment}-${var.service_name}-tx-outbox"
+  name   = var.instance_name
   role   = module.streams_handler.execution_role.id
   policy = <<EOF
 {
--- variables.tf
+++ variables.tf
@@ -1,17 +1,13 @@
-variable "project_name" {
-  type = string
-}
-
-variable "environment" {
-  type = string
-}
-
-variable "service_name" {
+variable "instance_name" {
   type = string
 }
 
 variable "source_table_stream_arn" {
   type = string
+}
+
+variable "kafka_brokers" {
+  type = list(string)
 }
 
 variable "kafka_user_id" {
@@ -20,4 +16,19 @@ variable "kafka_user_id" {
 
 variable "kafka_user_secret" {
   type = string
+}
+
+variable "kafka_auth_mechanism" {
+  type    = string
+  default = "plain"
+}
+
+variable "handler_layer" {
+  type    = string
+  default = "arn:aws:lambda:ap-southeast-2:340502884936:layer:dynamodb-kakfa-outbox:1"
+}
+
+variable "kafka_use_ssl" {
+  type    = bool
+  default = false
 }
